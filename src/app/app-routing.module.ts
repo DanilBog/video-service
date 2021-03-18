2d3b95a7cf3f0c8563db914042ab53c89de46f03
DanilBog <danilbogatyrev@gmail.com>
2021-03-18 19:09:12 +0400

    error fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChannelsComponent } from './channels/channels.component';
+import { FilmsComponent } from './films/films.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'channels', component: ChannelsComponent },
+  { path: 'films', component: FilmsComponent },
+  { path: '',   redirectTo: '/films', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChannelsComponent } from './channels/channels.component';
+import { FilmsComponent } from './films/films.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChannelsComponent,
+    FilmsComponent
   ],
   imports: [
     BrowserModule,
